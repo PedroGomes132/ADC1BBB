5026bc392b285a5e037c41738f8a648b32d9be91
PedroGomes132 <pedrogomeslima123@gmail.com>
2026-06-07 09:58:12 -0300

    adiciona stubs shared opcionais do rabbit

---
+++ api/public/shared/0e784cfb93/index.js
@@ -0,0 +1,13 @@
+System.register([], function (exports) {
+  "use strict";
+  return {
+    execute: function () {
+      exports("_$meta", {
+        name: "0e784cfb93",
+        version: "0.0.0-local",
+        alias: "local-optional-shared-stub"
+      });
+      exports("default", {});
+    }
+  };
+});
---
+++ api/public/shared/fa732ea0bd/index.js
@@ -0,0 +1,13 @@
+System.register([], function (exports) {
+  "use strict";
+  return {
+    execute: function () {
+      exports("_$meta", {
+        name: "fa732ea0bd",
+        version: "0.0.0-local",
+        alias: "local-optional-shared-stub"
+      });
+      exports("default", {});
+    }
+  };
+});
